298fa16f028238f9184d61b3331910d5932a180e
Nadia <105141058+NadiaMichelle@users.noreply.github.com>
2022-12-04 21:43:44 -0600

    mejora

--- Recursivo Nodos arboles.c
+++ Recursivo Nodos arboles.c
@@ -13,3 +13,20 @@ postorden(nodo)
   postorden(nodo.izquierda)
   postorden(nodo.derecha)
   imprime nodo.valor
+
+//funciones
+preorder(node)
+  if node == null then return
+  print node.value
+  preorder(node.left)
+  preorder(node.right)
+inorder(node)
+  if node == null then return
+  inorder(node.left)
+  print node.value
+  inorder(node.right)
+postorder(node)
+  if node == null then return
+  postorder(node.left)
+  postorder(node.right)
+  print node.value
